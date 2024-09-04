093be177f7a3a85de430047e4399d90aed675eec
Sebastien Le Maguer <sebastien.lemaguer@helsinki.fi>
2024-09-04 15:51:26 +0300

    refactor setup and README

--- README.md
+++ README.md
@@ -1,47 +1,60 @@
 # RobotVoice
+
 _Verified Set of TTS Voices for Common Robots_
 
-## Run synthesis server
+## Install server
+
+### With Docker
 To run locally, install Docker Desktop and run in the folder `RobotVoice`:
-```shell
-docker-compose up -d --build
-```
 
-You should now be able to test it: http://localhost:5000/
-
-## Run synthesis server on a server
-Provision at least a m5.xlarge instance on AWS. Install Docker and Docker Compose. Then run:
-```shell
+```sh
 git clone https://github.com/polvanrijn/RobotVoice
 cd RobotVoice
 docker-compose up -d --build
 ```
 
-If you don't want to use Docker, you can also install the dependencies manually:
-```shell
+### Without Docker
+
+If you don't want to use Docker, you can have to install the following pre-requisites: espeak unzip git wget
+
+When these packages are installed, you will have to configure a python environment (conda or other) which uses **python 3.10**.
+
+When the environment is activated, you can install the project using:
+
+```sh
 git clone https://github.com/polvanrijn/RobotVoice
 cd RobotVoice
 sh setup.sh
-# Set the port and number of workers based on the number of CPUs, set max-requests = 1 to avoid memory leak
-gunicorn server:app --worker-tmp-dir /dev/shm --workers=17 -b :5001 -t 600 --max-requests 1
 ```
 
+Finally you can start the server using:
+
+```sh
+# Set the number of workers based on the number of CPUs, set max-requests = 1 to avoid memory leak
+# The port is set to 5000 but can be changed as needed by modifying the value of "-b"
+gunicorn server:app --worker-tmp-dir /dev/shm --workers=1 -b :5000 -t 600 --max-requests 1
+```
+
+## Access the synthesis server
+
+You should now be able to test the server locally on http://localhost:5000/
+
 ## Scraping for robots
+
 Setup a virtual environment and install the dependencies:
-```shell
+```sh
 pip install opencv-python matplotlib beautifulsoup4 requests
 ```
 
 To create a json with all meta information from the robots listed at https://robots.ieee.org/robots/, run:
 ```
-python scrape_ieee.py 
+python scrape_ieee.py
 ```
 
-To select the images for the robots, run the following script. 
-```shell
+To select the images for the robots, run the following script.
+```sh
 mkdir "images"
-python select_robots.py 
+python select_robots.py
 ```
 
 An array with images will pop up. Either specify the image index (starts at 1!) or reject the image by entering `n`.
-
--- setup.sh
+++ setup.sh
@@ -1,33 +1,54 @@
-sudo apt-get update && sudo apt-get -f -y install espeak python3-pip unzip git wget gunicorn python3.10-venv
-python3.10 -m venv env
-source env/bin/activate
+#!/bin/bash
 
-# Upgrade pip
+# Install general python requirements
+echo "\n\n"
+echo "####################################################################"
+echo "## Installing python dependencies"
+echo "####################################################################"
 python -m pip install --upgrade pip
-git clone https://github.com/jaywalnut310/vits
-cd vits
+pip install -r requirements.txt
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install gdown
-gdown 11aHOlhnxzjpdWDpsz1vFDCzbeEfoIxru
-
-mv ../setup_vits.py setup.py
-
-mv monotonic_align ../monotonic_align
-cd ../monotonic_align
 pip install Cython
-python setup.py build_ext --inplace
-cd ..
 
-mkdir "VSTs"
-cd VSTs
-wget https://tal-software.com/downloads/plugins/Tal-Vocoder-2_64_linux-2.2.1.zip
-unzip Tal-Vocoder-2_64_linux-2.2.1.zip
-mv TAL-Vocoder-2/* .
-rm Tal-Vocoder-2_64_linux-2.2.1.zip
+# Install VITS
+echo -e "\n\n"
+echo "####################################################################"
+echo "## Installing VITS"
+echo "####################################################################"
+(
+    rm -rfv vits
+    git clone https://github.com/jaywalnut310/vits
+    cd vits
+    cp ../setup_vits.py setup.py # NOTE: add setup to be install VITS as a package
+    gdown 11aHOlhnxzjpdWDpsz1vFDCzbeEfoIxru
+    pip install -e .
+)
 
-cd ..
-pip install -r requirements.txt
+# Install monotonic align
+echo -e "\n\n"
+echo "####################################################################"
+echo "## Installing monotonic align"
+echo "####################################################################"
+(
+    rm -rfv monotonic_align/
+    mv vits/monotonic_align monotonic_align
+    cd monotonic_align
+    mkdir monotonic_align
+    python setup.py build_ext --inplace
+)
 
-cd vits
-pip install -e .
-cd ..
+# Install VST Vocoder plugin
+echo -e "\n\n"
+echo "####################################################################"
+echo "## Installing TAL Vocoder VST plugin"
+echo "####################################################################"
+(
+    rm -rfv VSTs/
+    mkdir "VSTs"
+    cd VSTs
+    wget https://tal-software.com/downloads/plugins/Tal-Vocoder-2_64_linux-2.2.1.zip
+    unzip Tal-Vocoder-2_64_linux-2.2.1.zip
+    mv TAL-Vocoder-2/* .
+    rm Tal-Vocoder-2_64_linux-2.2.1.zip
+)
